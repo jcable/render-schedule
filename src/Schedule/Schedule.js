83afdb6580fb4c0163abfce107dc50d50ceb4ed9
jcable <julian.cable@yahoo.com>
2019-10-18 16:45:27 +0100

    move api_key to environment

--- src/Schedule/Schedule.js
+++ src/Schedule/Schedule.js
@@ -5,12 +5,13 @@ import moment from 'moment-timezone';
 import template from "./Schedule.jsx";
 import bbc_world_service_2018_10_28 from "../bbc_world_service_2018_10_28";
 
-const api_key = "";
+const api_key = process.env.api_key;
 const sid = "bbc_world_service";
 
 function Schedule(props) {
   useEffect(() => {
     const d = moment.tz(props.date, "UTC").format("YYYY-MM-DD");
+    let l = moment.tz(props.date, props.zone).format("YYYY-MM-DD");
     async function fetchData() {
       let sched = [];
       let schedule = [];
@@ -21,13 +22,20 @@ function Schedule(props) {
         sched = jsondata['p:pips']['p:schedule']['p:schedule_services']['p:schedule_service']['p:items']['p:item'];
       }
       else {
-        const result = await axios(
-          `https://programmes.api.bbc.com/schedule?api_key=${api_key}&sid=${sid}&date=${d}`,
-        );
-        const xmldata = result.data;
-        const jsonstring = convert.xml2json(xmldata, {compact: true});
-        const jsondata = JSON.parse(jsonstring);
-        sched = jsondata['p:schedule']['p:item'];
+        const days = [ 
+          moment.tz(props.date, 'UTC').subtract(1, 'days').format('YYYY-MM-DD'),
+          d,
+          moment.tz(props.date, 'UTC').add(1, 'days').format('YYYY-MM-DD')
+        ];
+        for(const dd of days) {
+          const result = await axios(
+            `https://programmes.api.bbc.com/schedule?api_key=${api_key}&sid=${sid}&date=${dd}`,
+          );
+          const xmldata = result.data;
+          const jsonstring = convert.xml2json(xmldata, {compact: true});
+          const jsondata = JSON.parse(jsonstring);
+          sched = sched.concat(jsondata['p:schedule']['p:item']);
+        }
     }
     for(const row of sched) {
       const start = row["p:broadcast"]["p:accurate_time"]["_attributes"]["broadcast_start"]
@@ -38,13 +46,16 @@ function Schedule(props) {
       else {
         title += row["p:episode"]["p:title"]["_text"];
       }
-      schedule.push({
-        local: moment.tz(start, props.zone).format("HH:mm"),
-        utc: moment.tz(start, 'UTC').format("HH:mm"),
-        title: title,
-        synopsis: row["p:brand"]["p:synopses"]["p:synopsis"][0]["_text"],
-        start: moment.tz(start, 'UTC').format("HH:mm").valueOf()
-      });
+      const ld = moment.tz(start, props.zone).format("YYYY-MM-DD");
+      if(ld === l) {
+        schedule.push({
+          local: moment.tz(start, props.zone).format("HH:mm"),
+          utc: moment.tz(start, 'UTC').format("HH:mm"),
+          title: title,
+          synopsis: row["p:brand"]["p:synopses"]["p:synopsis"][0]["_text"],
+          start: moment.tz(start, 'UTC').format("HH:mm").valueOf()
+        });  
+      }
       } 
       setData(schedule);
     };
